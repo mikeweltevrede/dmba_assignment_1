aefedf7d6156efea3af7bfc53f04a0cc4ad02c70
danielbunnik <55542840+danielbunnik@users.noreply.github.com>
2019-09-20 12:41:14 +0200

    changes friday 20-9
    create all 45 svm's and start predict

--- svm.R
+++ svm.R
@@ -23,88 +23,42 @@ train_validation_split = function(train_all, num_samples, training_size=0.75) {
   return(list('train' = train, 'validation' = validation))
 }
 
-grid_search = function(train, validation,
-                       kernels = c("vanilladot", "rbfdot", "polydot"),
-                       c_vector = 10^(-3:3), sigma_vector = 10^(-8:2),
-                       degree_vector = 1:4) {
+grid_search = function(train, validation, c_vector = 10^(-3:3),
+                       sigma_vector = 10^(-8:2)) {
   
   optimal_accuracy = 0
   selected_parameters = list()
   
+  # Grid Search for RBF
   for (c_scalar in c_vector) {
     print(paste("C:", c_scalar))
     
-    # Grid Search for Vanilla
-    if ("vanilladot" %in% kernels) {
-      svm_lin = kernlab::ksvm(label ~ ., data = train, scaled = FALSE,
-                              kernel = "vanilladot", C = c_scalar)
-      evaluation_lin = predict(svm_lin, newdata = validation, type = "response")
-      cm_lin = caret::confusionMatrix(evaluation_lin, validation$label)
+    for (sigma in sigma_vector) {
+      print(paste("Sigma:", sigma))
       
-      current_accuracy = cm_lin$overall["Accuracy"]
+      svm = kernlab::ksvm(label ~ ., data = train, scaled = F,
+                              kernel = "rbfdot", C = c_scalar,
+                              kpar = list(sigma = sigma))
+      evaluation = predict(svm, newdata = validation, type = "response")
+      cm = caret::confusionMatrix(evaluation, validation$label)
+      
+      current_accuracy = cm$overall["Accuracy"]
       
       if (current_accuracy > optimal_accuracy) {
         optimal_accuracy = current_accuracy
         selected_parameters["C"] = c_scalar
+        selected_parameters["sigma"] = sigma
+        best_svm = svm
       }
-    }
-
-    # Check for RBF
-    if ("rbfdot" %in% kernels) {
-      for (sigma in sigma_vector) {
-        print(paste("Sigma:", sigma))
-        
-        svm_rbf = kernlab::ksvm(label ~ ., data = train, scaled = F,
-                                kernel = "rbfdot", C = c_scalar,
-                                kpar = list(sigma = sigma))
-        evaluation_rbf = predict(svm_rbf, newdata = validation,
-                                 type = "response")
-        cm_rbf = caret::confusionMatrix(evaluation_rbf, validation$label)
-        
-        current_accuracy = cm_rbf$overall["Accuracy"]
-        
-        if (current_accuracy > optimal_accuracy) {
-          optimal_accuracy = current_accuracy
-          selected_parameters["C"] = c_scalar
-          selected_parameters["sigma"] = sigma
-        }
-      }
-    }
-      
-      
-    
-    # Check for polynomial
-    if ("polydot" %in% kernels) {
-      for (degree in degree_vector) {
-        print(paste("degree:", degree))
-        
-        svm_poly = kernlab::ksvm(label ~ ., data = train, scaled = F,
-                                 kernel = "polydot", C = c_scalar,
-                                 kpar = list(degree = degree))
-        evaluation_poly = predict(svm_poly, newdata = validation, type = "response")
-        cm_poly = caret::confusionMatrix(evaluation_poly, validation$label)
-        
-        current_accuracy = cm_poly$overall["Accuracy"]
-        
-        if (current_accuracy > optimal_accuracy) {
-          optimal_accuracy = current_accuracy
-          selected_parameters["C"] = c_scalar
-          selected_parameters["degree"] = degree
-          
-        }
-      }  
     }
   }
   
   return(list("optimal_accuracy" = optimal_accuracy,
-              "selected_parameters" = selected_parameters))
+              "selected_parameters" = selected_parameters,
+              "svm" = best_svm))
 }
 
-# TODO: Change name
-my_svm = function(digit1, digit2, train, num_samples,
-                  kernels = c("vanilladot", "rbfdot", "polydot"),
-                  c_vector = 10^(-3:3), sigma_vector = 10^(-8:2),
-                  degree_vector = 1:4) {
+create_training_set = function(digit1, digit2, train, num_samples){
   idx_d1 <- which(train$label == digit1)
   idx_d2 <- which(train$label == digit2)
   
@@ -112,11 +66,36 @@ my_svm = function(digit1, digit2, train, num_samples,
   train_all$label <- factor(train_all$label) # Reset factor levels
   
   tvs = train_validation_split(train_all, num_samples)
+  
+  return(tvs)
+}
+  
+
+# TODO: Change name
+my_svm = function(digit1, digit2, train, num_samples, run_grid_search = FALSE,
+                  c_vector = 10^(-3:3), sigma_vector = 10^(-8:2)) {
+  
+  tvs = create_training_set(digit1, digit2, train, num_samples)
+  
   train = tvs$train
   validation = tvs$validation
   
-  results = grid_search(train, validation, kernels, c_vector, sigma_vector,
-                        degree_vector)
+  if (run_grid_search){
+    results = grid_search(train, validation, c_vector, sigma_vector)
+  } else {
+    c_scalar = c_vector[1]
+    sigma = sigma_vector[1]
+    
+    svm = kernlab::ksvm(label ~ ., data = train, scaled = F,
+                                   kernel = "rbfdot",
+                                   C = c_scalar,
+                                   kpar = list(sigma = sigma), prob.model=TRUE)
+    
+    results = list("optimal_accuracy" = NA, 
+                   "selected_parameters" = list("C" = c_scalar, 
+                                                "sigma" = sigma),
+                   "svm" = svm)
+  }
   
   return(results)
 }
@@ -124,31 +103,27 @@ my_svm = function(digit1, digit2, train, num_samples,
 #### Run functions ####
 
 train = import_data("data/mnist_train.csv")
-# test = import_data("data/mnist_test.csv")
+test = import_data("data/mnist_test.csv")
 
 # testing
 c_vector = 10^(-2:2)
 sigma_vector = 10^(-4:1)
-degree_vector = 1:3
 
 accuracies = list()
 
 for (other_digit in 0:9) {
-  
   if (other_digit == 5) {
     next
   }
   
   optimal_parameters = my_svm(5, other_digit, train, num_samples = 1000,
-                              kernels = c('rbfdot'), c_vector = c_vector,
-                              sigma_vector = sigma_vector,
-                              degree_vector = degree_vector)
+                              run_grid_search = TRUE, c_vector = c_vector,
+                              sigma_vector = sigma_vector)
   
   print(paste("For digit", other_digit, "the accuracy is:", optimal_parameters$optimal_accuracy))
   print("-------------")
   
   accuracies[as.character(other_digit)] = optimal_parameters$optimal_accuracy
-  
 }
 
 print(accuracies)
@@ -160,15 +135,44 @@ print(paste("Least similar:", least_similar))
 print(paste("Most similar:", most_similar))
 # TODO: try to see if we can make it recognise multiple maxima (low priority)
 
-
-
-
 #### 2. ####
+svms = list()
+
+for (i in 0:8){
+  for (j in (i+1):9){
+    
+    digit_combo = paste(i, "_",j)
+    svms[digit_combo] = my_svm(i, j, train, num_samples = 1000, 
+                               c_vector = c_vector, 
+                               sigma_vector = sigma_vector)$svm
+    
+    print(paste("svm created for", i, "and", j))
+  }
+}
 
 
 
+num_rows = dim(test)[1]
 
+labels = test$label
 
+row = 5
 
+for (i in 0:8) {
+  for (j in (i+1):9) {
+    
+    pixels = test[row, -1]
+    digit_combo = paste(i, "_",j)
+    
+    svm_test = 3 # TODO
+    
+    kernlab::predict(svm_test, test[, -1], type="response")
+            
+  }
+}
 
+kernlab::predict(svm_test, test[, -1], type="response")
+svm_test = my_svm(0, 1, train, num_samples = 1000, 
+                  c_vector = 0.1, 
+                  sigma_vector = sigma_vector)$svm
 

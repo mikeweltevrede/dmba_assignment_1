bea949316c50ae58c5dee3d386be31fe262fe577
daniel <danielbunnik@outlook.com>
2019-09-26 20:35:07 +0200

    Update deep_learning.R

--- deep_learning.R
+++ deep_learning.R
@@ -5,6 +5,8 @@ library(caret)
 library(kernlab)
 library(e1071)
 library(neuralnet)
+library(ANN2)
+
 
 import_data = function(path){
   data = read.csv(path, stringsAsFactors = F, header = F)
@@ -14,7 +16,18 @@ import_data = function(path){
 }
 
 ## Functions
+train_validation_split = function(train_all, num_samples, training_size = 0.75){
+  
+  sample_indices <- sample(1:nrow(train_all), num_samples)
+  training_samples <- floor(num_samples*training_size)
+  
+  train <- train_all[sample_indices[1:training_samples], ]
+  validation <- train_all[sample_indices[(training_samples + 1):num_samples], ]
+  
+  return(list('train' = train, 'validation' = validation))
+}
 
+  
 
 
 
@@ -23,4 +36,7 @@ train = import_data("data/mnist_train.csv")
 test = import_data("data/mnist_test.csv")
 
 #### 5 ####
+train_auto.data <- train_validation_split(train, num_samples = 1000, training_size = 0.75)
+ann784_200 <- neuralnet(autoencoder~0+1+2+3+4+5+6+7+8+9, data = train_auto.data, hidden = 1, learningrate = 0.05, act.fct = "logistic")
+plot(ann782_200)
 

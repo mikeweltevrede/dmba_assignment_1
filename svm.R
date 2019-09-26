84bd60cd1e6cd708306ad8fa9573a7d307190a76
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2019-09-26 08:07:04 +0200

    Prototype final majority vote

--- svm.R
+++ svm.R
@@ -82,23 +82,23 @@ my_svm = function(digit1, digit2, train, num_samples, run_grid_search = FALSE,
   train = tvs$train
   validation = tvs$validation
   
-  if (run_grid_search){
+  if (run_grid_search) {
     results = grid_search(train, validation, c_vector, sigma_vector)
   } else {
-    c_scalar = c_vector[1]
-    sigma = sigma_vector[1]
+    # If grid search is not run, take the C=10 and sigma=10^-7 (which we note
+    # are the most common options, as derived from an earlier run grid search)
+    c_scalar = 10
+    sigma = 10^(-7)
     
-    svm = kernlab::ksvm(label ~ ., data = train, scaled = F,
-                        kernel = "rbfdot",
-                        C = c_scalar,
-                        kpar = list(sigma = sigma), prob.model=TRUE)
+    svm = kernlab::ksvm(label ~ ., data = train, scaled = F, kernel = "rbfdot",
+                        C = c_scalar, kpar = list(sigma = sigma),
+                        prob.model = TRUE)
     
     results = list("optimal_accuracy" = NA, 
                    "selected_parameters" = list("C" = c_scalar, 
                                                 "sigma" = sigma),
                    "svm" = svm)
   }
-  
   return(results)
 }
 
@@ -108,8 +108,6 @@ train = import_data("data/mnist_train.csv")
 test = import_data("data/mnist_test.csv")
 
 #### 1 ####
-
-# testing
 c_vector = 10^(-3:3)
 sigma_vector = 10^(-8:2)
 
@@ -126,139 +124,136 @@ for (other_digit in 0:9) {
                               run_grid_search = TRUE, c_vector = c_vector,
                               sigma_vector = sigma_vector)
   
-  print(paste("For digit", other_digit, "the accuracy is:",
-              optimal_parameters$optimal_accuracy))
-  print(paste("For digit", other_digit, "the best value for C is:",
-              optimal_parameters$selected_parameters$C,
-              "and the best value for sigma is:",
-              optimal_parameters$selected_parameters$sigma))
+  print(paste("Other digit:", other_digit))
+  print(paste("Accuracy:", optimal_parameters$optimal_accuracy,
+              "Optimal C:", optimal_parameters$selected_parameters$C,
+              "Optimal sigma:", optimal_parameters$selected_parameters$sigma))
   print("-------------")
   
   accuracies[as.character(other_digit)] = optimal_parameters$optimal_accuracy
 }
 
-
-print(accuracies)
-
+# TODO: try to see if we can make it recognise multiple maxima (low priority)
 least_similar = accuracies[which.max(accuracies)]
 most_similar = accuracies[which.min(accuracies)]
 
+print(paste("All accuracies:", accuracies))
 print(paste("Least similar:", least_similar))
 print(paste("Most similar:", most_similar))
-# TODO: try to see if we can make it recognise multiple maxima (low priority)
 
 
-#### 2. ####  ##TODO: probeer 5 hieruit te halen
-svms = list()
-accuracies_ij = list()
+#### 2. ####
+parameters = list("C" = list("0_1" = , "0_2" = , "0_3" = , "0_4" = ,
+                             "0_5" = , "0_6" = , "0_7" = , "0_8" = ,
+                             "0_9" = , "1_2" = , "1_3" = , "1_4" = ,
+                             "1_5" = , "1_6" = , "1_7" = , "1_8" = ,
+                             "1_9" = , "2_3" = , "2_4" = , "2_5" = ,
+                             "2_6" = , "2_7" = , "2_8" = , "2_9" = ,
+                             "3_4" = , "3_5" = , "3_6" = , "3_7" = ,
+                             "3_8" = , "3_9" = , "4_5" = , "4_6" = ,
+                             "4_7" = , "4_8" = , "4_9" = , "5_6" = ,
+                             "5_7" = , "5_8" = , "5_9" = , "6_7" = ,
+                             "6_8" = , "6_9" = , "7_8" = , "7_9" = ,
+                             "8_9" = ),
+                  "sigma" = list("0_1" = , "0_2" = , "0_3" = , "0_4" = ,
+                                 "0_5" = , "0_6" = , "0_7" = , "0_8" = ,
+                                 "0_9" = , "1_2" = , "1_3" = , "1_4" = ,
+                                 "1_5" = , "1_6" = , "1_7" = , "1_8" = ,
+                                 "1_9" = , "2_3" = , "2_4" = , "2_5" = ,
+                                 "2_6" = , "2_7" = , "2_8" = , "2_9" = ,
+                                 "3_4" = , "3_5" = , "3_6" = , "3_7" = ,
+                                 "3_8" = , "3_9" = , "4_5" = , "4_6" = ,
+                                 "4_7" = , "4_8" = , "4_9" = , "5_6" = ,
+                                 "5_7" = , "5_8" = , "5_9" = , "6_7" = ,
+                                 "6_8" = , "6_9" = , "7_8" = , "7_9" = ,
+                                 "8_9" = ))
 
-for (i in 0:8){
-  for (j in (i+1):9){
-    
-    digit_combo = paste(i, "_", j)
-    svms[digit_combo] = my_svm(i, j, train, num_samples = 1000,
-                               run_grid_search = TRUE, c_vector = c_vector, #heb de grid_search nu even op TRUE gezet 
-                               sigma_vector = sigma_vector)$svm 
-    
-    ## ---- ik heb dit hieronder (tot de volgende ---) toegevoegd, 
-    #           waarschijnlijk zal ik het wel weer verkeerd begrepen hebben haha,
-    #           maar ik dacht dat dit mogelijk een oplossing kan zijn.
-    
-    print(paste("svm created for", i, "and", j))
-    print(paste("For digit", i, "and", j, "the accuracy is:",
-                optimal_parameters$optimal_accuracy))
-    print(paste("For digit", i, "_", j, "the best value for C is:",
-                optimal_parameters$selected_parameters$C,
-                "and the best value for sigma is:",
-                optimal_parameters$selected_parameters$sigma))
-    print("-------------")
-    
-    accuracies_ij[as.character(digit_combo)] = optimal_parameters$optimal_accuracy
+##TODO: probeer 5 hieruit te halen
+create_svms = function(train, num_samples, run_grid_search = FALSE,
+                       c_vector = c(), sigma_vector = c(),
+                       parameters = c()) {
+  
+  svms = list()
+  
+  for (i in 0:8) {
+    for (j in (i + 1):9) {
+      
+      print(paste("Creating SVM for", i, "and", j, "..."))
+      
+      digit_combo = paste0(i, "_", j)
+      
+      if (!run_grid_search) {
+        # Use predefined parameters
+        c_vector = parameters[["C"]][digit_combo] # TODO: Check if this works
+        sigma_vector = parameters[["sigma"]][digit_combo] # TODO: idem
+      }
+      
+      svms[digit_combo] = my_svm(i, j, train, num_samples = num_samples,
+                                 run_grid_search = run_grid_search,
+                                 c_vector = c_vector,
+                                 sigma_vector = sigma_vector)$svm 
+      
+      print(paste("SVM created for", i, "and", j))
+      print(paste("Accuracy:", optimal_parameters$optimal_accuracy,
+                  "Optimal C:", optimal_parameters$selected_parameters$C,
+                  "Optimal sigma:", optimal_parameters$selected_parameters$sigma))
+      print("-------------")
+    }
   }
+  return(svms)
 }
 
-num_rows = dim(test)[1]
-labels = test$label
-for (row in 1:num_rows){
-  label = labels[row]
-  for (svm in svms){
-    predict()
-  }
-}
-test
-
-sum_accuracies_ij = do.call(sum, accuracies_ij)
-acc_mvt = sum_accuracies_ij/length(accuracies_ij)
-print(paste("The accuracy of the Majority Vote System is:", acc_mvt))
+svms = create_svms(train, num_samples = 1000, run_grid_search = FALSE,
+                   parameters = parameters)
 
 ## -----
 
+majority_vote = function(svms, test) {
 
-num_rows = dim(test)[1]
-
-labels = test$label
-
-svms_test = list()
-
-svms_test["0 _ 1"] =  my_svm(0, 1, train, num_samples = 1000, 
-                           c_vector = 0.1, 
-                           sigma_vector = sigma_vector)$svm
-
-svms_test["0 _ 2"] =  my_svm(0, 2, train, num_samples = 1000, 
-                           c_vector = 0.1, 
-                           sigma_vector = sigma_vector)$svm
-
-svms_test["3 _ 4"] =  my_svm(3, 4, train, num_samples = 1000, 
-                           c_vector = 0.1, 
-                           sigma_vector = sigma_vector)$svm
-
-
-
-# TODO: Spaties?
-preds = list()
-
-for (dc in c("0 _ 1", "0 _ 2", "3 _ 4")) {
+  preds = list()
   
-  prediction = kernlab::predict(svms_test[[dc]], test[, -1],
-                                type="response")
-  prediction[prediction==0] = substr(dc, 1, 1)
-  prediction[prediction==1] = substr(dc, nchar(dc), nchar(dc))
-  
-  preds[[dc]] = prediction
-}
-
-preds = list()
-
-for (i in 0:8) {
-  for (j in (i + 1):9) {
-    digit_combo = paste(i, "_", j)
-    
-    prediction = kernlab::predict(svms[[digit_combo]], test[, -1],
-                                  type="response")
-    # Als er uit pred een 0 komt, is het de eerste digit en anders de tweede
-    prediction[prediction==0] = i
-    prediction[prediction==1] = j
-    
-    preds[digit_combo] = prediction
+  # This for-loop gets predictions for all 45 SVMs
+  for (i in 0:8) {
+    for (j in (i + 1):9) {
+      
+      digit_combo = paste0(i, "_", j)
+      
+      # kernlab::predict() predicts the entire matrix...
+      prediction = kernlab::predict(svms[[digit_combo]], test[, -1],
+                                    type = "response")
+      
+      # and returns a 0 for the first class (i) and a 1 for the second class (j)
+      prediction[prediction == 0] = i
+      prediction[prediction == 1] = j
+      
+      # Save the result in a list
+      preds[digit_combo] = prediction
+    }
   }
-}
+  
+  # Unlist these predictions in a matrix for column-wise comparison
+  preds_matrix = matrix(unlist(preds), ncol = dim(test)[1], byrow = TRUE)
+  
+  # Initialise list of winners
+  winners = c()
+  
+  # And retrieve winners from the votes. If there is a tie, say between d1 and
+  # d2, the which.max() function takes the digit that was voted for first.
+  for (col in 1:length(preds_matrix)) {
+    winner = names(which.max(table(preds_matrix[, 1])))
+    winners = c(winners, winner)
+  }
 
-preds_matrix = matrix(unlist(preds), ncol = 10000, byrow=TRUE)
-
-num_cols = length(preds_matrix)
-
-winners = c()
-for (col in 1:num_cols) {
-  winner = names(which.max(table(preds_matrix[, 1])))
-  winners = c(winners, winner)
+  return(winners)
 }
 
 # tapply gebruiken?
-boolean_test = (winners == labels)
-acc_mvs = sum(boolean_test) / dim(test)[1]
-
+winners = majority_vote(svms, test)
+score = (winners == test$label)
+acc_mvs = sum(score) / dim(test)[1]
 
 #### 3 ####
+# TODO
 # hier kunnen we toch gewoon de svms[digit_combo]$accuracy pakken?
 # Want dan hebben we alle accuracies op een rijtje en kunnen we vervolgens 
 # de 3 (bijv.) slechtste en beste pakken toch?
@@ -286,15 +281,19 @@ library(neuralnet)
 
 #TODO krijg hem niet aan de praat
 
-tvs_nn <- train_validation_split(train, num_samples = 1000, training_size = 0.75)
+tvs_nn <- train_validation_split(train, num_samples = 1000,
+                                 training_size = 0.75)
 nn_traindata <- tvs_nn$train
+nn_validation <- tvs_nn$validation
+rm(tvs_nn)
+
 ann_traindata <- nn_traindata[-1]
 digits = 0:9
 
-for (H in seq(5,20,5)){
+for (H in seq(5,20,5)) {
   
-  ann <- neuralnet(digits~01+02, data = nn_traindata, hidden = H, act.fct = "logistic",
-                   linear.output = FALSE)
+  ann <- neuralnet::neuralnet(digits ~ 01 + 02, data = nn_traindata, hidden = H,
+                              act.fct = "logistic", linear.output = FALSE)
   
   plot(ann)
 }

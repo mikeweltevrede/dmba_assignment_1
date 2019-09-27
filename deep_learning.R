579e8e0cab15453b4df3ce3adfc189e3b3233a22
daniel <danielbunnik@outlook.com>
2019-09-27 09:11:17 +0200

    Update deep_learning.R

--- deep_learning.R
+++ deep_learning.R
@@ -5,8 +5,7 @@ library(caret)
 library(kernlab)
 library(e1071)
 library(neuralnet)
-library(ANN2)
-
+library(keras)
 
 import_data = function(path){
   data = read.csv(path, stringsAsFactors = F, header = F)
@@ -16,18 +15,16 @@ import_data = function(path){
 }
 
 ## Functions
-train_validation_split = function(train_all, num_samples, training_size = 0.75){
+train_validation_split = function(train_all, num_samples, training_size=0.75) {
   
   sample_indices <- sample(1:nrow(train_all), num_samples)
-  training_samples <- floor(num_samples*training_size)
+  training_samples = floor(num_samples*training_size)
   
   train <- train_all[sample_indices[1:training_samples], ]
   validation <- train_all[sample_indices[(training_samples + 1):num_samples], ]
   
   return(list('train' = train, 'validation' = validation))
 }
-
-  
 
 
 
@@ -36,7 +33,17 @@ train = import_data("data/mnist_train.csv")
 test = import_data("data/mnist_test.csv")
 
 #### 5 ####
-train_auto.data <- train_validation_split(train, num_samples = 1000, training_size = 0.75)
-ann784_200 <- neuralnet(autoencoder~0+1+2+3+4+5+6+7+8+9, data = train_auto.data, hidden = 1, learningrate = 0.05, act.fct = "logistic")
-plot(ann782_200)
+sample_indices <- sample(1:nrow(train), 100)
+training_samples = floor(100*0.75)
+train1 <- train[sample_indices[1:training_samples], ]
+validation1 <- train[sample_indices[(training_samples + 1):100], ]
+#ik kreeg de train_validation op de een of andere manier niet voor elkaar
+
+train_x <- train[,2:785] %>% as.matrix()
+train_y <- train[,1] %>% 
+  keras::to_categorical()
+
+test_x <- validation[,785] %>% as.matrix()
+
+
 

6a21912afda669e81ee3908097a63b9e2f3821c6
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2019-09-28 19:18:15 +0200

    Implement random projection

--- dimension_reduction.R
+++ dimension_reduction.R
@@ -16,8 +16,9 @@ library(caret)
 library(kernlab)
 library(e1071)
 library(keras)
+library(RandPro)
 
-#### Define functions ####
+# Import functions from files
 source("svm_functions.R")
 source("dimension_reduction_functions.R")
 
@@ -28,27 +29,6 @@ test = import_data("data/mnist_test.csv")
 labels_train = data.frame("label" = train$label)
 labels_test = data.frame("label" = test$label)
 
-#### Average Pooling ####
-# To debug - shouldn't need to transpose outcome?
-start = proc.time()
-train_pooled = train[, -1] %>%
-  apply(1, average_pooling) %>%
-  t() %>%
-  as.data.frame() %>%
-  cbind(labels_train, ., row.names = NULL)
-end = proc.time()
-time_pool_train = end - start
-
-start = proc.time()
-test_pooled = test[, -1] %>%
-  apply(1, average_pooling) %>%
-  t() %>%
-  as.data.frame() %>%
-  cbind(labels_test, ., row.names = NULL)
-end = proc.time()
-time_pool_test = end - start
-
-# Create SVMs
 parameters = list(
   "C" = list("0_1" = 1, "0_2" = 1, "0_3" = 10, "0_4" = 100, "0_5" = 1,
              "0_6" = 1, "0_7" = 1, "0_8" = 1, "0_9" = 10, "1_2" = 10,
@@ -75,69 +55,149 @@ parameters = list(
                  "6_7" = 10^(-7), "6_8" = 10^(-6), "6_9" = 10^(-7),
                  "7_8" = 10^(-7), "7_9" = 10^(-6), "8_9" = 10^(-7)))
 
-# Start the clock for creating SVMs
-# Time original creating function:
+#### Random Projection ####
+start = proc.time()
+train_proj = random_projection(train)
+end = proc.time()
+time_proj_train = end - start
+
+start = proc.time()
+test_proj = random_projection(test)
+end = proc.time()
+time_proj_test = end - start
+
+#### Average Pooling ####
+start = proc.time()
+train_pooled = train[, -1] %>%
+  apply(1, average_pooling) %>%
+  t() %>%
+  as.data.frame() %>%
+  cbind(labels_train, ., row.names = NULL)
+end = proc.time()
+time_pool_train = end - start
+
+start = proc.time()
+test_pooled = test[, -1] %>%
+  apply(1, average_pooling) %>%
+  t() %>%
+  as.data.frame() %>%
+  cbind(labels_test, ., row.names = NULL)
+end = proc.time()
+time_pool_test = end - start
+
+#### Time: Create SVMs ####
+# Original
 start <- proc.time()
 svms = create_svms(train, num_samples = 1000, run_grid_search = FALSE,
                    parameters = parameters)
 end <- proc.time()
-
 time_creation_original <- end - start
 
+# Random Projection
+start <- proc.time()
+svms_pooled = create_svms(train_proj, num_samples = 1000,
+                          run_grid_search = FALSE, parameters = parameters)
+end <- proc.time()
+time_creation_proj = end - start
+
+# Average Pooling
 start <- proc.time()
 svms_pooled = create_svms(train_pooled, num_samples = 1000,
                           run_grid_search = FALSE, parameters = parameters)
 end <- proc.time()
-
 time_creation_pooled = end - start
 
-# Majority Vote
+#### Time: Cast and Count Votes ####
 # Original
 start = proc.time()
-  mvs = majority_vote(svms, test)
-  winners = mvs$winners
+mvs = majority_vote(svms, test)
+winners = mvs$winners
 
-  score = (winners == labels_test)
-  acc_mvs = sum(score) / dim(test)[1]
+score = (winners == labels_test)
+acc_mvs = sum(score) / dim(test)[1]
 end = proc.time()
 time_mvs_original = end - start
 
-# MVS for pooled
+# Random Projection
 start = proc.time()
-  mvs_pooled = majority_vote(svms_pooled, test_pooled)
-  winners_pooled = mvs_pooled$winners
-  
-  score_pooled = (winners == labels_test)
-  acc_mvs_pooled = sum(score_pooled) / dim(test_pooled)[1]
+mvs_proj = majority_vote(svms, test_proj)
+winners_proj = mvs_proj$winners
+
+score_proj = (winners_proj == labels_test)
+acc_mvs_proj = sum(score_proj) / dim(test_proj)[1]
+end = proc.time()
+time_mvs_proj = end - start
+
+# Average Pooling
+start = proc.time()
+mvs_pooled = majority_vote(svms_pooled, test_pooled)
+winners_pooled = mvs_pooled$winners
+
+score_pooled = (winners_pooled == labels_test)
+acc_mvs_pooled = sum(score_pooled) / dim(test_pooled)[1]
 end = proc.time()
 time_mvs_pooled = end - start
 
+# Print times
 print("Times:")
-print("Creating pooled data:")
-print(time_pool_train)
-print(time_pool_test)
+print("Creating randomly projected data:")
+print(paste("Train:", time_proj_train["user.self"]))
+print(paste("Test:", time_proj_test["user.self"]))
+
+print("Creating average pooled data:")
+print(paste("Train:", time_pool_train["user.self"]))
+print(paste("Test:", time_pool_test["user.self"]))
 
 print("Creating SVMs:")
-print(time_creation_original)
-print(time_creation_pooled)
+print(paste("Original:", time_creation_original["user.self"]))
+print(paste("Random Projection:", time_creation_proj["user.self"]))
+print(paste("Average Pooling:", time_creation_pooled["user.self"]))
 
-print("MVS:")
-print(time_mvs_original)
-print(time_mvs_pooled)
+print("Casting and Counting Votes:")
+print(paste("Original:", time_mvs_original["user.self"]))
+print(paste("Random Projection:", time_mvs_proj["user.self"]))
+print(paste("Average Pooling:", time_mvs_pooled["user.self"]))
 
 print("Total time:")
 print(paste("Original:", time_creation_original["user.self"]
             + time_mvs_original["user.self"]))
-print(paste("Pooled:", time_pool_train["user.self"]
+print(paste("Random Projection:", time_proj_train["user.self"]
+            + time_proj_test["user.self"]
+            + time_creation_proj["user.self"]
+            + time_mvs_proj["user.self"]))
+print(paste("Average Pooling:", time_pool_train["user.self"]
             + time_pool_test["user.self"]
             + time_creation_pooled["user.self"]
             + time_mvs_pooled["user.self"]))
 
 print("-----------------")
-print("Accuracy:")
+print("Accuracy MVS:")
 print(paste("Original:", acc_mvs))
-print(paste("Pooled:", acc_mvs_pooled))
+print(paste("Random Projection:", acc_mvs_proj))
+print(paste("Average Pooling:", acc_mvs_pooled))
 
+# Checking individual accuracies looking at each digit separately...
+accuracies_mvs = list()
+accuracies_mvs_proj = list()
+accuracies_mvs_pooled = list()
 
-#### Random projection ####
+for (i in 0:9) {
+  accuracy = sum(
+    score[labels_test == i])/length(labels_test[labels_test == i])
+  accuracies_mvs[[as.character(i)]] = accuracy
+  
+  accuracy = sum(
+    score_proj[labels_test == i])/length(labels_test[labels_test == i])
+  accuracies_mvs_proj[[as.character(i)]] = accuracy
+  
+  accuracy = sum(
+    score_pooled[labels_test == i])/length(labels_test[labels_test == i])
+  accuracies_mvs_pooled[[as.character(i)]] = accuracy
+}
 
+print("Individual accuracies:")
+accuracies_separate = rbind(unlist(accuracies_mvs), 
+                            unlist(accuracies_mvs_proj),
+                            unlist(accuracies_mvs_pooled))
+rownames(accuracies_separate) = c("Original", "Random Projection", "Average Pooling")
+print(accuracies_separate)
